5cde38df55481aa5d2a629b832e5eecab20aed40
KumailP <kumailpirzada@gmail.com>
2018-04-21 17:12:20 +0500

    Separated MySQL Connection in a different class

--- EpicLMSDesktopApp/formLogin.cs
+++ EpicLMSDesktopApp/formLogin.cs
@@ -17,74 +17,44 @@ namespace EpicLMSDesktopApp
         private void btnLogin_Click(object sender, EventArgs e)
         {
 
-            MySqlConnection con = new MySqlConnection("SERVER=localhost; DATABASE=epiclms; UID=root; PASSWORD=root");
-            try
-            {
-                con.Open();
+
+            MySqlConnection con = MySQLConnection.openCon();
                 
 
-                //MessageBox.Show("Connection Open ! ");
+            //MessageBox.Show("Connection Open ! ");
 
-                string email = txtEmail.Text.Trim();
-                string password = txtPassword.Text;
+            string email = txtEmail.Text.Trim();
+            string password = txtPassword.Text;
 
-                string query = "SELECT * from student WHERE email='" + email + "'";
-                MySqlCommand cmd = new MySqlCommand(query, con);
-                MySqlDataReader dataReader = cmd.ExecuteReader();
-                DataTable dtb1 = new DataTable();
-                dtb1.Load(dataReader);
-                //cmd.ExecuteNonQuery();
+            string query = "SELECT * from student WHERE email='" + email + "'";
+            MySqlCommand cmd = new MySqlCommand(query, con);
+            MySqlDataReader dataReader = cmd.ExecuteReader();
+            DataTable dtb1 = new DataTable();
+            dtb1.Load(dataReader);
+            //cmd.ExecuteNonQuery();
 
-                //new SqlDataAdapter(query, con).Fill(dtb1);
-                if (dtb1.Rows.Count == 1)
+            //new SqlDataAdapter(query, con).Fill(dtb1);
+            if (dtb1.Rows.Count == 1)
+            {
+                bool doesmatch = BCrypt.CheckPassword(password, dtb1.Rows[0][4].ToString());
+                if (doesmatch)
                 {
-                    bool doesmatch = BCrypt.CheckPassword(password, dtb1.Rows[0][4].ToString());
-                    if (doesmatch)
-                    {
-                        formMain frm = new formMain();
-                        this.Hide();
-                        frm.Show();
-                    }
-                    else
-                    {
-                        MessageBox.Show("Unable to login!");
-                    }
+                    formMain frm = new formMain();
+                    this.Hide();
+                    frm.Show();
                 }
                 else
                 {
-                    MessageBox.Show("User not found");
+                    MessageBox.Show("Unable to login!");
                 }
-                
-                
             }
-            catch (MySqlException ex)
+            else
             {
-                switch (ex.Number)
-                {
-                    case 0:
-                        MessageBox.Show("Cannot connect to server.  Contact administrator");
-                        break;
-
-                   case 1045:
-                        MessageBox.Show("Invalid username/password, please try again");
-                        break;
-                }
-                
+                MessageBox.Show("User not found");
             }
 
 
-
-
-
-
-            try
-            {
-                con.Close();
-            }
-            catch (MySqlException ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+            MySQLConnection.closeCon(con);
         }
 
 
