73e0c6f4fc6117696aaa4fcd364129a529db105d
KumailP <kumailpirzada@gmail.com>
2018-04-21 17:38:42 +0500

    Fixed mySQL file

---
+++ EpicLMSDesktopApp/MySQLConnection.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace EpicLMSDesktopApp
+{
+    class MySQLConnection
+    {
+        public static MySqlConnection openCon()
+        {
+            MySqlConnection con = new MySqlConnection("SERVER=localhost; DATABASE=epiclms; UID=root; PASSWORD=root");
+            try
+            {
+                con.Open();
+                return con;
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        MessageBox.Show("Invalid username/password, please try again");
+                        break;
+                }
+                return con;
+            }
+
+
+
+
+            /*
+            try
+            {
+                con.Close();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+             */
+        }
+        
+        public static bool closeCon(MySqlConnection con)
+        {
+            try
+            {
+                con.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+    }
+}
